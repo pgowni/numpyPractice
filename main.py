601c5034194f3c7a1e642c1001fa3a6ce715c255
pgowni <65507706+pgowni@users.noreply.github.com>
2024-09-16 14:32:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
+featuresData_df = pd.read_csv('data/Features data set.csv')
+salesData_df = pd.read_csv('data/sales data-set.csv')
+storesData_df = pd.read_csv('data/stores data-set.csv')
 
+# Get the top 5 entries for each DataFrame
+top5_df1 = featuresData_df.head(5)
+top5_df2 = salesData_df.head(5)
+top5_df3 = storesData_df.head(5)
